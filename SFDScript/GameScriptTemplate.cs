fcac0f513306f1362d3ae7084535d282b66ff9c7
NearHuscarl <near.huscarl@gmail.com>
2019-02-09 13:18:16 +0700

    add profile converter to convert IProfile object into c# code string

--- SFDScript/GameScriptTemplate.cs
+++ SFDScript/GameScriptTemplate.cs
@@ -1,6 +1,6 @@
 ﻿using SFDGameScriptInterface;
 
-namespace SFDScript.Script2
+namespace SFDScript
 {
     public partial class GameScript : GameScriptInterface
     {
@@ -9,12 +9,28 @@ namespace SFDScript.Script2
         /// </summary>
         public GameScript() : base(null) { }
 
-        #region Startup Calls
-
         public void OnStartup()
         {
+            // The game will always call the following method "public void OnStartup()" during a map start (or script activates). 
+            // No triggers required. This is run before triggers that activate on startup (and before OnStartup triggers).
+            Game.ShowPopupMessage("OnStartup is run when the map or script is started.");
+            
+            // This is a breakpoint
+            // System.Diagnostics.Debugger.Break();
         }
 
-        #endregion
+        public void AfterStartup()
+        {
+            // The game will always call the following method "public void AfterStartup()" after a map start (or script activates). 
+            // No triggers required. This is run after triggers that activate on startup (and after OnStartup triggers).
+
+        }
+
+        public void OnShutdown()
+        {
+            // The game will always call the following method "public void OnShutdown()" before a map restart (or script deactivates). 
+            // Perform some cleanup here or store some final information to Game.Data if needed.
+
+        }
     }
 }
